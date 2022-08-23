9e01635e8f87be1fc7478ab3d002aaa7ff2488e4
Alex Hayes <alexpghayes@gmail.com>
2022-08-23 13:31:45 -0500

    Add some plotting utils

---
+++ R/plotting.R
@@ -0,0 +1,80 @@
+#' Calculate the expected adjacency matrix
+#'
+#' @param model A [directed_factor_model()] or an [undirected_factor_model()]
+#'   object.
+#' @param ... Unused.
+#'
+#' @return The expected value of the adjacency matrix, conditional on the
+#'   latent factors `X` and `Y` (if the model is directed).
+#' @export
+#'
+expectation <- function(model, ...) {
+  UseMethod("expectation")
+}
+
+#' @rdname expectation
+#' @export
+expectation.undirected_factor_model <- function(model, ...) {
+  model$X %*% tcrossprod(model$S, model$X)
+}
+
+#' @rdname expectation
+#' @export
+expectation.directed_factor_model <- function(model, ...) {
+  model$X %*% tcrossprod(model$S, model$Y)
+}
+
+#' Plot (expected) adjacency matrices
+#'
+#' @inheritParams expectation
+#' @param A A [matrix()], [Matrix::Matrix()] or [Matrix::sparseMatrix()] object.
+#'
+#' @return A [ggplot2::ggplot2()] plot.
+#' @export
+#'
+plot_expectation <- function(model) {
+  EA <- as.matrix(expectation(model))
+  plot_dense_matrix(EA)
+}
+
+#' @rdname plot_expectation
+#' @import ggplot2
+#' @export
+plot_dense_matrix <- function(A, ...) {
+  long <- dplyr::as_tibble(A, rownames = "row")
+  long <- tidyr::gather(long, col, value, -row)
+  long <- dplyr::mutate_all(long, as.numeric)
+
+  ggplot(long, aes(x = col, y = row, fill = value)) +
+    geom_raster() +
+    scale_y_reverse() +
+    theme_minimal() +
+    labs(
+      fill = "Expected edges"
+    ) +
+    theme(
+      axis.title = element_blank()
+    )
+}
+
+#' @rdname plot_expectation
+#' @export
+plot_sparse_matrix <- function(A) {
+
+  stopifnot(inherits(A, "sparseMatrix"))
+
+  A <- methods::as(A, "dgCMatrix")
+
+  ggplot(summary(A), aes(x = i, y = j, fill = as.factor(x))) +
+    geom_raster() +
+    scale_fill_grey() +
+    scale_y_reverse() +
+    theme_void(16) +
+    labs(
+      fill = "Edges"
+    ) +
+    theme(
+      axis.title = element_blank(),
+      axis.text = element_blank()
+    )
+}
